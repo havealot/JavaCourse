edc832cd49efc76dedc8fdc262b2c32415ad8732
Abdelrahman Mahmoud <20693584+Abdoam@users.noreply.github.com>
2017-08-04 16:13:07 +0200

    Create Menu.java

---
+++ Task1/Menu.java
@@ -0,0 +1,32 @@
+public class Menu {
+
+	public static void main(String [] args){
+ 		
+		int dollar = 1;
+		switch(dollar){   // don't use break
+    
+		}
+    
+	}
+  
+}
+
+
+/*
+
+dollar = 1
+-----------
+the Fish
+
+dollar = 2
+----------
+the fish
+And rice
+
+dollar = 3
+----------
+the fish
+And rice
+And bread
+
+*/
