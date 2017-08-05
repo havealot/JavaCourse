7fb4a1acf32ca65cb1e9893d874380c2ca0e5aa7
Ammar <mu7ammad.ammar@gmail.com>
2017-08-05 13:48:31 +0200

    Update Draw.java

--- Task1/Draw.java
+++ Task1/Draw.java
@@ -1,12 +1,21 @@
 // 10 point
-public class Draw {
-
+class Draw {
+ 
 	public static void main(String [] args){
  
-		System.out.printf(); // make change here
-    
+		System.out.print(
+			String.format("%65s\n", "*").replace(" ", "*") + 
+			String.format("*%64s\n", "*") + 
+			String.format("**%63s\n", "**") + 
+			String.format("***%62s\n", "***") + 
+			String.format("****%61s\n", "****") + 
+			String.format("*****%60s\n", "*****") + 
+			String.format("**********%55s\n", "**********") + 
+			String.format("%65s\n", "*").replace(" ", "*")
+			); // make change here
+ 
 	}
-  
+ 
 }
 
 
