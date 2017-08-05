33256391b5945463851cf359872f31117829a02b
Ammar <mu7ammad.ammar@gmail.com>
2017-08-05 02:33:34 +0200

    Update Menu.java
    Task 1 : menu

--- Task1/Menu.java
+++ Task1/Menu.java
@@ -1,17 +1,33 @@
 // 20 point
-public static void main(String [] args){
- 		
-		int dollar = 1;
-		if( ){ // make change from here
-		switch(dollar){   // don't use break and don't repeat words (fish,rice,bread) and use ternary
+/* package whatever; // don't place package name! */
 
-		}
-		}
+import java.util.*;
+import java.lang.*;
+import java.io.*;
 
-    
-	}
-  
+public class Main{
+	public static void main(String [] args){
+	 		
+			int dollar = 4;
+			if(dollar>0){ // make change from here
+				dollar=(dollar>3?3:dollar);
+				switch(dollar){   // don't use break and don't repeat words (fish,rice,bread) and use ternary
+					case 3:
+						System.out.print("the bread\nAnd");
+					case 2:
+						System.out.print(dollar==2?"the ":" ");
+						System.out.print("rice\nAnd");
+					case 1:
+						System.out.print(dollar==1?"the ":" ");
+						System.out.println("fish");
+				}
+			}
+	
+	    
+		}
 }
+  
+
 /*
 dollar = 1
 -----------
